733d7a326966ded5810a1f4c7598b303107eb751
kenenisa gebi <keno7van@gmail.com>
2025-04-23 20:32:02 +0300

    Day six commit CP leetcode_problem

---
+++ Day_six/climbing_s.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+           return n
+
+        a, b = 1, 2  
+        for i in range(3, n + 1):
+            a, b = b, a + b  
+
+        return b
+            
+
+
+# Example usage:
+solution = Solution()
+result = solution.climbStairs(5)    
+print(result)  
---
+++ Day_six/intersection_r.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        counts = Counter(nums1)
+        result = []
+
+        for num in nums2:
+            if counts[num] > 0:
+                result.append(num)
+                counts[num] -= 1
+
+        return result
+            
+
+
+# Example usage:
+solution = Solution()
+result = solution.intersect([1, 3, 2, 1], [1, 2, 1])
+print(result)  
